3d838ecbee1d1b25a558aff48c639959b976fcc5
Archer2x <archer@mtu.edu>
2025-02-20 18:20:14 -0500

    Update HelloWorld.java
    Updated the class with comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        // Edited this by adding a comment to the class
         System.out.println("Hello World!");
     }
 }
